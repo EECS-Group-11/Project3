4d10765db1567f3cf29274f159e82c569d4439ad
Ethan Grantz <e284g507@ku.edu>
2020-10-25 23:17:10 -0500

    Ethan Demo Code

--- src/PieceLogic.js
+++ src/PieceLogic.js
@@ -1,28 +1,75 @@
 
 class Piece {
-    constructor() {
-        //either pass in piece type, randomly generate it in here,
-        //or maybe make subclasses?
+    constructor(type) {
+        this.shape = type;
+        //[row,col]
+        this.coords = Array(4).fill(null).map(x => x = [0,0]);
     }
 
     moveDown() {
-        //no need to move up in tetris
+        this.coords.forEach(x => x[0] += 1);
     }
 
     moveHor(dir) {
         //-1 = left, 1 = right
+        this.coords.forEach(x => x[1] += dir);
     }
 
     rotate() {
-        //each piece has a different center of rotation uhoh
+        //each piece has a different center of rotation uhoh, see image for reference
+        //https://vignette.wikia.nocookie.net/tetrisconcept/images/3/3d/SRS-pieces.png/revision/latest?cb=20060626173148
     }
 }
 
+class I extends Piece {
+    constructor() {
+        super("I");
+        this.coords = [[0,5],[1,5],[2,5],[3,5]];
+        this.color = "blue";
+    }
+}
+
+class O extends Piece {
+    constructor() {
+        super("O");
+        this.coords = [[0,4],[0,5],[1,4],[1,5]];
+        this.color = "yellow"
+    }
+}
+
+class L extends Piece {
+    constructor() {
+        super("L");
+    }
+}
+
+class J extends Piece {
+    constructor() {
+        super("J");
+    }
+}
+
+class T extends Piece {
+    constructor() {
+        super("T");
+    }
+}
+
+class S extends Piece {
+    constructor() {
+        super("S");
+    }
+}
+
+class Z extends Piece {
+    constructor() {
+        super("Z");
+    }
+}
+
+//returns object of a random type of piece
 function getRandomPiece() {
-    //depends on how Piece constructor works
-    return new Piece()
-    //or
-    let types = ["I", "J","L","S","O","Z","T"]
-    return new Piece(types[Math.floor(Math.random() * 7)]);
-    //or the subclasses method
+    //let types = [I,O,L,J,T,S,Z];
+    let types = [I,O];
+    return new types[Math.floor(Math.random() * types.length)]();
 }
--- src/main.js
+++ src/main.js
@@ -3,28 +3,85 @@ function main() {
     //obviously these variables might be moved or declared elsewhere, but these are 
     //some basic elements that will likely be in the game
 
-    board = Array(20).fill(null).map(x => x = Array(10).fill(0));
-    currentPiece = getRandomPiece();
+    board = Array(24).fill(null).map(x => x = Array(10).fill("#ffffff"));
+    currentPiece =  getRandomPiece();
     nextPiece = getRandomPiece();
     heldPiece = null;
 }
 
-function onCollisionDetected() {
-    if (/*left or right wall collision*/true) {
-        //prevent left or right movement
+//obviously will need to be refactored some, just some demo code
+function updateGraphics() {
+    //put currentPiece on board
+    for (const coord of currentPiece.coords) {
+        board[coord[0]][coord[1]] = currentPiece.color;
     }
-    else {
-        currentPiece = nextPiece;
-        nextPiece = getRandomPiece()
+    //draw board
+    for (let i = 4; i < 24; i++) {
+        for (let j = 0; j < 10; j++) {
+            document.querySelector('#r'+ (i-4) + 'c' + j).style.backgroundColor = board[i][j];
+        }
     }
 }
 
 function downkey(e) {
     if (e.key !== 'F5' && e.key !== 'F12') e.preventDefault();
     //will need to add collision detection on movement functions
-    if (e.key === 'ArrowDown') currentPiece.moveDown();
-    if (e.key === 'ArrowLeft') currentPiece.moveHor(-1);
-    if (e.key === 'ArrowRight') currentPiece.moveHor(1);
+    if (e.key === 'ArrowDown') {
+        let temp = currentPiece.coords.map(x => [x[0] + 1, x[1]]);
+        //remove from board so it doesnt attempt to collide with itself
+        for (const c of currentPiece.coords) {
+            board[c[0]][c[1]] = "#ffffff";
+        }
+        for (const c of temp) {
+            //avoid drawing things that are above screen
+            if (c[0] < 0) continue;
+            //if collision
+            if (c[0] > 23 || board[c[0]][c[1]] !== "#ffffff") {
+                //redraw on board
+                for (const c of currentPiece.coords) {
+                    board[c[0]][c[1]] = currentPiece.color;
+                }
+                //swap pieces
+                currentPiece = nextPiece;
+                nextPiece = getRandomPiece();
+                return;
+            }
+        } 
+        currentPiece.moveDown();
+    }
+    if (e.key === 'ArrowLeft') {
+        let temp = currentPiece.coords.map(x => [x[0], x[1] - 1]);
+        for (const c of currentPiece.coords) {
+            board[c[0]][c[1]] = "#ffffff";
+        }
+        for (const c of temp) {
+            if (c[0] < 0) continue;
+            if (c[1] < 0 || board[c[0]][c[1]] !== "#ffffff") {
+                for (const c of currentPiece.coords) {
+                    board[c[0]][c[1]] = currentPiece.color;
+                }
+                return;
+            }
+        }
+        currentPiece.moveHor(-1);
+    }
+    if (e.key === 'ArrowRight') {
+        let temp = currentPiece.coords.map(x => [x[0], x[1] + 1]);
+        for (const c of currentPiece.coords) {
+            board[c[0]][c[1]] = "#ffffff";
+        }
+        for (const c of temp) {
+            if (c[0] < 0) continue;
+            if (c[1] > 9 || board[c[0]][c[1]] !== "#ffffff") {
+                for (const c of currentPiece.coords) {
+                    board[c[0]][c[1]] = currentPiece.color;
+                }
+                return;
+            }
+        }
+        currentPiece.moveHor(1);
+    }
+    //swap held piece and currentPiece
     if (e.key === 'z') {//swap heldPiece and currentPiece
         let temp = heldPiece;
         heldPiece = currentPiece;
@@ -32,6 +89,7 @@ function downkey(e) {
     }
     if (e.key === ' ') currentPiece.rotate();
     //maybe add a hard drop function too?
+    updateGraphics();
 }
 
 document.addEventListener("DOMContentLoaded", main);
